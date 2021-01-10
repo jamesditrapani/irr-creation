b7a299061136f56d677cd3489c14ddfe513195c5
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2021-01-11 01:15:12 +1000

    Removed "-" in Package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="irr-generator", 
-    version="0.0.1",
+    version="0.0.3",
     author="James Di Trapani",
     author_email="james@ditrapani.com.au",
     description="Generate IRR Route Objects at scale quickly and without error",
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jamesditrapani/irr-generator",
     packages=setuptools.find_packages(),
+    install_requires=[
+      "requests>==2.25.1",
+      "netaddr>==0.8.0"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
@@ -20,7 +24,7 @@ setuptools.setup(
     ],
     entry_points={
       'console_scripts': [
-        'irrgenerator = irrgenerator:main',
+        'irrgenerator = irrgenerator.irrgenerator:main',
       ],
     },
     python_requires='>=3.6',
